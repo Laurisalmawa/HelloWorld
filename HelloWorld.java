2c6505410a95913afaf8b692417ed061af8ba228
Laura <laurisalmawa05gmail.com>
2024-04-25 13:17:02 +0200

    Archivo Java, My name is Laura

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    private int age = 23;
+    private String nombre = "Laura";
+    
+    public static void main(String[]args) {
+	HelloWorld h = new HelloWorld();
+        System.out.println("Hi, my name is" + h.nombre + " and I'm 19 years old");
+    }
+}
